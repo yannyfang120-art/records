843255c23900daa522b220a4bd7f5c8918af5e76
Yanny Fang <yannyfang120@gmail.com>
2026-04-22 16:12:22 +0300

    Better way to show errors

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import Flask
-from flask import abort, make_response, redirect, render_template, request, session
+from flask import abort, flash, make_response, redirect, render_template, request, session
 
 import config
 import db
@@ -63,11 +63,13 @@ def add_image():
 
     file = request.files["image"]
     if not file.filename.endswith(".png"):
-        return "VIRHE: väärä tiedostomuoto"
+        flash("VIRHE: väärä tiedostomuoto")
+        return redirect("/images/" + str(item_id))
 
     image = file.read()
     if len(image) > 100 * 1024:
-        return "VIRHE: liian suuri kuva"
+        flash("VIRHE: liian suuri kuva")
+        return redirect("/images/" + str(item_id))
 
     items.delete_images(item_id)
 
@@ -154,7 +156,8 @@ def update_item():
 
     review_points = float(request.form["review_points"])
     if review_points < 1 or review_points > 10:
-        return "VIRHE: arvostelun pitää olla 1-10"
+        flash("VIRHE: arvostelun pitää olla 1-10")
+        return redirect("/item/" + str(item_id))
 
     review_points = round(review_points, 1)
     review_points_str = f"{review_points:.1f}"
@@ -215,7 +218,8 @@ def create_item():
 
     review_points = float(request.form["review_points"])
     if review_points < 1 or review_points > 10:
-        return "VIRHE: arvostelun pitää olla 1-10"
+        flash("VIRHE: arvostelun pitää olla 1-10")
+        return redirect(f"/item/{item_id}")
 
     user_id = session["user_id"]
 
@@ -241,14 +245,16 @@ def create():
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
+        flash("VIRHE: salasanat eivät ole samat")
+        return redirect("/register")
 
     try:
         users.create_user(username, password1)
     except sqlite3.IntegrityError:
-        return "Virhe: tunnus on jo varattu"
+        flash("Virhe: tunnus on jo varattu")
+        return redirect("/register")
 
-    return "Tunnus luotu"
+    return redirect("/")
 
 #tiedon etsiminen
 
@@ -314,7 +320,8 @@ def login():
             session["username"] = username
             return redirect("/")
         else:
-            return "VIRHE: väärä tunnus tai salasana"
+            flash("VIRHE: väärä tunnus tai salasana")
+            return redirect("/login")
 
 
 
